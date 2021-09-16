3502883f6b0e00f5dc799af61395a49b0950a6f9
djv <83076587+djv-mo@users.noreply.github.com>
2021-09-16 03:52:07 +0200

    Update test_urls.py

--- bills/billsapi/test/test_urls.py
+++ bills/billsapi/test/test_urls.py
@@ -7,68 +7,68 @@ billitem = BillsItemsFactory()
 
 
 def test_bill_list_reverse():
-    assert reverse('bills:bills-list') == '/bills/'
+    assert reverse('bills:bills-list') == '/endpoint/bills/'
 
 
 def test_bill_list_resolve():
-    assert resolve('/bills/').view_name == 'bills:bills-list'
+    assert resolve('/endpoint/bills/').view_name == 'bills:bills-list'
 
 
 def test_bill_detail_reverse():
     url = reverse('bills:bills-detail', kwargs={'pk': bill.id})
-    assert url == f'/bills/{bill.id}/'
+    assert url == f'/endpoint/bills/{bill.id}/'
 
 
 def test_bill_detail_resolve():
-    url = f'/bills/{bill.id}/'
+    url = f'/endpoint/bills/{bill.id}/'
     assert resolve(url).view_name == 'bills:bills-detail'
 
 
 def test_bill_items_list_reverse():
     url = reverse('bills:items-list', kwargs={'pk': bill.id})
-    assert url == f'/bills/{bill.id}/items/'
+    assert url == f'/endpoint/bills/{bill.id}/items/'
 
 
 def test_bill_items_list_resolve():
-    url = f'/bills/{bill.id}/items/'
+    url = f'/endpoint/bills/{bill.id}/items/'
     assert resolve(url).view_name == 'bills:items-list'
 
 
 def test_item_update_reverse():
     url = reverse('bills:items-update', kwargs={'pk': billitem.id})
-    assert url == f'/items/{billitem.id}/'
+    assert url == f'/endpoint/items/{billitem.id}/'
 
 
 def test_item_update_resolve():
-    url = f'/items/{billitem.id}/'
+    url = f'/endpoint/items/{billitem.id}/'
     assert resolve(url).view_name == 'bills:items-update'
 
 
 def test_export_bill_reverse():
     url = reverse('bills:items-csv', kwargs={'pk': bill.id})
-    assert url == f'/bills/{bill.id}/export/'
+    assert url == f'/endpoint/bills/{bill.id}/export/'
 
 
 def test_export_bill_resolve():
-    url = f'/bills/{bill.id}/export/'
+    url = f'/endpoint/bills/{bill.id}/export/'
     assert resolve(url).view_name == 'bills:items-csv'
 
 
 def test_archive_bill_reverse():
     url = reverse('bills:archived-bills')
-    assert url == f'/archived/'
+    assert url == f'/endpoint/archived/'
 
 
 def test_archive_bill_resolve():
-    url = f'/archived/'
+    url = f'/endpoint/archived/'
     assert resolve(url).view_name == 'bills:archived-bills'
 
 
 def test_archive_unarchive_bill_reverse():
     url = reverse('bills:archive-unarchieve', kwargs={'pk': bill.id})
-    assert url == f'/bills/{bill.id}/archive/'
+    assert url == f'/endpoint/bills/{bill.id}/archive/'
 
 
 def test_archive_unarchive_bill_resolve():
-    url = f'/bills/{bill.id}/archive/'
+    url = f'/endpoint/bills/{bill.id}/archive/'
     assert resolve(url).view_name == 'bills:archive-unarchieve'
